c6b70c2afa58257cfd3f5e0b8db6018701008259
Padmanabha Das <padmanabhadas9647@gmail.com>
2023-06-10 23:06:00 +0530

    animation effect added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,8 +5,11 @@ import Heart from '/src/assets/heart.png'
 import hero_image from '/src/assets/hero_image.png'
 import hero_image_back from '/src/assets/hero_image_back.png'
 import calories from '/src/assets/calories.png'
+import {motion} from "framer-motion";
 
 export const Hero = () => {
+    const transition = {type: 'spring', duration: 3}
+
     return (
         <div className='hero'>
             <div className='blur hero-blur'></div>
@@ -14,7 +17,10 @@ export const Hero = () => {
                 <Header/>
                 {/* the best ad */}
                 <div className='the-best-ad'>
-                    <div></div>
+                    <motion.div
+                        initial={{left: '238px'}}
+                        whileInView={{left: '8px'}}
+                        transition={{...transition, type: 'tween'}}/>
                     <span>the best fitness club in the town</span>
                 </div>
 
@@ -24,9 +30,7 @@ export const Hero = () => {
                         <span className='stroke-text'>Shape </span>
                         <span>Your</span>
                     </div>
-                    <div>
-                        <span>Ideal body</span>
-                    </div>
+                    <div><span>Ideal body</span></div>
                     <div className='hero-text'>
                         <span>
                             In here we will help you to shape and build your ideal body and live up your life to fullest
@@ -60,24 +64,37 @@ export const Hero = () => {
 
             <div className='right-h'>
                 <button className='btn'>Join Now</button>
-                <div className='heart-rate'>
+                <motion.div
+                    initial={{right: '-1rem'}}
+                    whileInView={{right: '4rem'}}
+                    transition={transition}
+                    className='heart-rate'>
                     <img src={Heart} alt=''/>
                     <span>Heart Rate</span>
                     <span>116 bpm</span>
-                </div>
+                </motion.div>
 
                 {/* hero images */}
                 <img src={hero_image} alt='' className='hero-image'/>
-                <img src={hero_image_back} alt='' className='hero-image-back'/>
+                <motion.img
+                    initial={{right: '11rem'}}
+                    whileInView={{right: '20rem'}}
+                    transition={transition}
+                    src={hero_image_back} alt=''
+                    className='hero-image-back'/>
 
                 {/* calories */}
-                <div className='calories'>
+                <motion.div
+                    initial={{right: '37rem'}}
+                    whileInView={{right: '28rem'}}
+                    transition={transition}
+                    className='calories'>
                     <img src={calories} alt=''/>
                     <div>
                         <span>Calories Burned</span>
                         <span>220 kcal</span>
                     </div>
-                </div>
+                </motion.div>
             </div>
         </div>
     )
--- src/components/Testimonials/Testimonials.jsx
+++ src/components/Testimonials/Testimonials.jsx
@@ -3,10 +3,13 @@ import './Testimonials.css'
 import {testimonialsData} from "../../data/TestimonialsData.jsx";
 import leftArrow from '../../assets/leftArrow.png'
 import rightArrow from '../../assets/rightArrow.png'
+import {motion} from "framer-motion";
 
 export const Testimonials = () => {
     const [selected, setSelected] = useState(0);
     const tlength = testimonialsData.length
+
+    const transition = {type: 'spring', duration: 3}
 
     return (
         <div className='testimonials'>
@@ -14,9 +17,12 @@ export const Testimonials = () => {
                 <span>Testimonials</span>
                 <span className='stroke-text'>What they</span>
                 <span>say about us</span>
-                <span>
-                    {testimonialsData[selected].review}
-                </span>
+                <motion.span
+                    key={selected}
+                    initial={{opacity: 0, x: -100}}
+                    animate={{opacity: 1, x: 0}}
+                    exit={{opacity: 0, x: 100}}
+                    transition={transition}>{testimonialsData[selected].review}</motion.span>
                 <span>
                     <span style={{color: 'var(--orange)'}}>
                     {testimonialsData[selected].name}
@@ -25,9 +31,24 @@ export const Testimonials = () => {
                 </span>
             </div>
             <div className='right-t'>
-                <div></div>
-                <div></div>
-                <img src={testimonialsData[selected].image} alt=''/>
+                <motion.div
+                    initial={{opacity: 1, x: -100}}
+                    transition={{...transition, duration: 2}}
+                    whileInView={{opacity: 1, x: 0}}
+                />
+                <motion.div
+                    initial={{opacity: 0, x: 100}}
+                    transition={{...transition, duration: 2}}
+                    whileInView={{opacity: 1, x: 0}}
+                />
+                <motion.img
+                    key={selected}
+                    initial={{opacity: 0, x: 100}}
+                    animate={{opacity: 1, x: 0}}
+                    exit={{opacity: 0, x: -100}}
+                    transition={transition}
+                    src={testimonialsData[selected].image}
+                    alt=''/>
 
                 <div className='arrows'>
                     <img
